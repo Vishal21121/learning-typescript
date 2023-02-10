df971de1d704dbc7ce3336816987e175944f49db
Vishal21121 <vs84077000@gmail.com>
2023-02-10 23:14:38 +0530

    added number and boolean type code

--- 01-intro/VariableMe.js
+++ 01-intro/VariableMe.js
@@ -4,3 +4,10 @@ var greetings = "Hello Vishal";
 var myNum = 6;
 greetings.toLowerCase();
 console.log(greetings);
+// number
+// whenever we are initializing a variable at the time of declaring it then its not needed to state the type at that time typescript automatically understands the data type and it is known as type inference
+var userId = 334455;
+// now we can use only number function
+userId.toFixed();
+// boolean
+var isLoggedIn = false;
--- 01-intro/VariableMe.ts
+++ 01-intro/VariableMe.ts
@@ -3,4 +3,15 @@ let myNum = 6
 greetings.toLowerCase()
 console.log(greetings)
 
+// number
+// whenever we are initializing a variable at the time of declaring it then its not needed to state the type at that time typescript automatically understands the data type and it is known as type inference
+let userId = 334455
+
+// now we can use only number function
+userId.toFixed()
+
+// boolean
+let isLoggedIn: boolean = false
+  
+
 export {}
