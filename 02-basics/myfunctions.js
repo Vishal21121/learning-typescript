b920b479db16a6722b9dfbca437c2d53ea71c0b1
Vishal21121 <vs84077000@gmail.com>
2023-03-18 17:29:57 +0530

    added code for type aliase

--- 02-basics/myfunctions.js
+++ 02-basics/myfunctions.js
@@ -1,6 +1,6 @@
 "use strict";
 // let say we want number and string has been passed as argument so it will create a problem hence we can pass the data type which should be passed as argument in the following manner.
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 // here we are specifying the return type by passing :data-type before { and after )
 function addTwo(num) {
     return num + 2;
@@ -18,7 +18,7 @@ var val = getUpper("hello");
 console.log(val);
 signUpUser("vishal", "vishal.dev", false);
 loginUser("vishal", "vishal.dev");
-// this is the situation where we have to pass more than one data type hence we will learn ho to hanlde these cases in future.
+// this is the situation where we have to pass more than one data type hence we will learn ho to handle these cases in future.
 function getValue(myVal) {
     if (myVal > 5) {
         return true;
@@ -40,3 +40,10 @@ function consoleError(errMsg) {
 function handleError(errMsg) {
     throw new Error(errMsg);
 }
+// by this we can make create alias for boolean as bool and we can either use boolean or bool 
+// type bool = boolean;
+// here we are taking parameter as User and returning with User data type as well.
+function createUser(user) {
+    return user;
+}
+createUser({ name: "", email: "", isActive: true });
