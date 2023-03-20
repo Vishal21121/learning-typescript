6c6403ab6441cc618836edaada6ebe1775ba944c
Vishal21121 <vs84077000@gmail.com>
2023-03-20 16:52:19 +0530

    added code for class in typescript

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
-console.log("typescript is here");
-console.log("typescript is amazing");
+// console.log("typescript is here")
+// console.log("typescript is amazing")
+class User {
+    constructor(email, name) {
+        this.city = "Kolkata";
+        this.email = email;
+        this.name = name;
+    }
+}
+const vishal = new User("vishal@gmail.com", "vishal");
+//* we will get error in doing this as this variable is marked as readonly
+// vishal.city = "Kolkata"
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,2 +1,18 @@
-console.log("typescript is here")
-console.log("typescript is amazing")
+// console.log("typescript is here")
+// console.log("typescript is amazing")
+
+class User{
+    //* we have to declare the variables which we want to use in this class
+    email:string
+    name:string
+    readonly city:string = "Kolkata"
+    constructor(email:string,name:string){
+        this.email = email
+        this.name = name
+    }
+}
+
+const vishal = new User("vishal@gmail.com","vishal")
+
+//* we will get error in doing this as this variable is marked as readonly
+// vishal.city = "Kolkata"
