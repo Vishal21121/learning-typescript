a81edf9b9a43ce09950a239c0457d1f58993e8f3
Vishal21121 <vs84077000@gmail.com>
2023-03-22 09:09:22 +0530

    added code for type narrowing

---
+++ 03purets/dist/detection.js
@@ -0,0 +1,28 @@
+"use strict";
+//* actually what we are dealing here is that while dealing with types we need to deal with some special cases like empty string in the below example
+function detectType(val) {
+    if (typeof val === "string") {
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log("please provide ID");
+        return;
+    }
+    id.toLowerCase();
+}
+//* We wrapped the entire body of the function in a truthy check, but this has a subtle downside: we may no longer be handling the empty string case correctly.
+function printAll(strs) {
+    if (strs) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        }
+        else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
---
+++ 03purets/src/detection.ts
@@ -0,0 +1,30 @@
+//* actually what we are dealing here is that while dealing with types we need to deal with some special cases like empty string in the below example
+
+function detectType(val:number | string){
+    if(typeof val === "string"){
+        return val.toLowerCase()
+    }
+    return val + 3
+}
+
+
+function provideId(id:string | null){
+    if(!id){
+        console.log("please provide ID")
+        return
+    }
+    id.toLowerCase()
+}
+
+//* We wrapped the entire body of the function in a truthy check, but this has a subtle downside: we may no longer be handling the empty string case correctly.
+function printAll(strs: string | string[] | null) {
+    if (strs) {
+      if (typeof strs === "object") {
+        for (const s of strs) {
+          console.log(s);
+        }
+      } else if (typeof strs === "string") {
+        console.log(strs);
+      }
+    }
+  }
